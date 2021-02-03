407cbc31a402649848c2e80e845e06cc7b0d58ca
@maverick0701 <madhavanandcp070@gmail.com>
2021-02-03 14:53:20 +0530

    add syles for key

--- proj_ipod/src/KeyPad.js
+++ proj_ipod/src/KeyPad.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForward } from '@fortawesome/free-solid-svg-icons';
+import { faBackward} from '@fortawesome/free-solid-svg-icons';
+import { faPlay} from '@fortawesome/free-solid-svg-icons';
 class KeyPad extends React.Component{
    
     render(){
         return(
             <div className='KeyPad'>
                 <div className='KeyCircle'>
-                    <div className='Key1'>key 1</div>
-                    <div className="Key2">key 2</div>
-                    <div className="Key3">key 3</div>
-                    <div className="Key4">key 4</div>
+                    <div className='Key1' style={{color:'silver' }}>Menu</div>
+                    <div className="Key2" style={ { fontSize: 25 } }>  <FontAwesomeIcon icon={faBackward} /></div>
+                    <div className="Key3" style={ { fontSize: 25 } }>  <FontAwesomeIcon icon={faForward} /></div>
+                    <div className="Key4" style={ { fontSize: 25 } }><FontAwesomeIcon icon={faPlay} /></div>
                 </div>
             </div>
         );
