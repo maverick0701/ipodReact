59d93059c127859bc431e3367626eccda211a258
@maverick0701 <madhavanandcp070@gmail.com>
2021-02-05 18:32:45 +0530

    added rotate

--- proj_ipod/src/Case.js
+++ proj_ipod/src/Case.js
@@ -9,8 +9,10 @@ class Case extends React.Component{
        super();
        this.state={
            currentScreen:'Home',
-           activeLink:1
+           activeLink:1,
+           angle:0
        }
+    
    }
    componentDidMount(){
   
@@ -19,8 +21,54 @@ class Case extends React.Component{
     var myRegion = new ZingTouch.Region(touchArea);
    const rotate=new ZingTouch.Rotate();
 
-   myRegion.bind(touchArea, rotate, function(e){
-	console.log(e.detail.angle,e.detail.distanceFromLast);
+   myRegion.bind(touchArea, rotate, (e)=>{
+       
+        if(this.state.angle==0)
+        {
+            this.state.angle=e.detail.angle;   
+        }
+        else{
+            if(e.detail.angle-this.state.angle>15)
+            {
+                
+                this.state.angle=e.detail.angle;
+                var currentLink=this.state.activeLink;
+                if(currentLink==1)
+                {
+                    currentLink=4;
+                    
+                }
+                else
+                {
+                    currentLink--;
+                }
+                this.setState({
+                    activeLink:currentLink
+                })
+                console.log(currentLink);
+            }
+            if(e.detail.angle-this.state.angle<-15)
+            {
+                this.state.angle=e.detail.angle;
+                var currentLink=this.state.activeLink;
+                if(currentLink==4)
+                {
+                    currentLink=1;
+                    
+                }
+                else
+                {
+                    currentLink++;
+                }
+               
+                this.setState({
+                    activeLink:currentLink
+                })
+            }
+           
+        }
+      
+	// console.log(e.detail.angle,e.detail.distanceFromLast);
 });
    
    }
