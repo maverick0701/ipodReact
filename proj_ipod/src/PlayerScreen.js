ba8d75830441c24ce56788e748e47715b3f3b20e
@maverick0701 <madhavanandcp070@gmail.com>
2021-02-14 04:09:28 +0530

    added song player

--- proj_ipod/src/PlayerScreen.js
+++ proj_ipod/src/PlayerScreen.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Allsong from './AllSong'
-import Player from './Player'
+import Allsong from './AllSong';
+import Player from './Player';
+import SongPlayer from './SongPlayer';
+import musicPlayer from './SongPlayer'
 const PlayerScreen =(props)=>{
     const {activeLink,selectedPlayer,prevSelectedPlayer}=props;
-   
+   console.log(selectedPlayer);
     
         return(
             <div >
              
-               {prevSelectedPlayer=='music' && selectedPlayer['title']}<br />
-               {prevSelectedPlayer=='music' &&selectedPlayer['src']}
+               {prevSelectedPlayer=='music' && 
+               <SongPlayer 
+               activeLink={activeLink}
+               selectedPlayer={selectedPlayer}
+               prevSelectedPlayer={prevSelectedPlayer}
+               />}<br />
+               
                {prevSelectedPlayer!='music' && 
                <Player 
                activeLink={activeLink}
---
+++ proj_ipod/src/SongPlayer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './musicPlayer.css'
+const SongPlayer =(props)=>{
+    const {selectedPlayer}=props;
+  
+    
+        return(
+            <div id='musicPlayer' >
+             
+               
+            </div>
+        )
+}
+
+export default SongPlayer;
