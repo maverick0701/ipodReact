89abb95ea722d0bbfb85fb39182a6ecf725f0b55
@maverick0701 <madhavanandcp070@gmail.com>
2021-02-14 04:18:00 +0530

    added song image

--- proj_ipod/src/SongPlayer.js
+++ proj_ipod/src/SongPlayer.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './musicPlayer.css'
 const SongPlayer =(props)=>{
     const {selectedPlayer}=props;
-  
+    console.log(selectedPlayer.img)
     
         return(
             <div id='musicPlayer' >
-             
+                <div id='img'>
+                    <img src=  {selectedPlayer.img} />
+                </div>
+                <div id='controls'>
+
+                </div>
                
             </div>
         )
